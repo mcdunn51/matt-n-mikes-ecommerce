7cd954e4127451874786f8269dc3d544ad0293e3
Matt Dunning <Matt.dunning@SVG.local>
2019-02-18 17:09:39 +0000

    half way through homepage

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MainNavBar from './Components/MainNavBar/MainNavBar';
+import ThreeBrands from './Components/ThreeBrands/ThreeBrands';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MainNavBar/>
+        <ThreeBrands/>
       </div>
     );
   }
