3bdb2ecc3464740e81a74c0943baf19b8442c104
Michael Mountford <Michael.Mountford@SVG.local>
2019-02-26 10:40:20 +0000

    started backend again in working condition right now with cors installed and ready to test

--- Oauth/mysite/mysite/settings.py
+++ Oauth/mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'oauth2_provider',
     'rest_framework',
+    'django_mysql',
     'data',
     # 'corsheaders',
 ]
---
+++ backEnd/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\michael.mountford\onedrive\programming\matt-mikes-practice\backend\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
