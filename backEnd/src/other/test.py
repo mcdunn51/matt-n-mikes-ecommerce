35d48d5db94455aebba3f4cfba9dbeca08c65382
Michael Mountford <Michael.Mountford@SVG.local>
2019-03-05 16:31:54 +0000

    end of day update

--- backEnd/src/other/test.py
+++ backEnd/src/other/test.py
@@ -10,20 +10,20 @@ import json, os
 # authentication = requests.get(r'http://192.168.20.118:8000/o/token/')
 
 IP = '192.168.20.89:8000'
-token = 'bKhxgSmPL4Hl5QHyJayOmIrDBtbAoN'
+token = 'RzazVFDkRrWvJzpUCUyBsoabf5cQQS'
 
 try:
     test = 'Productlist endpoint'
-    print('<<< Testing %s >>>' % test)
+    print(r'http://' + str(IP) + r'/Productlist/?access_token='+str(token)+r'&format=json')
     authentication = requests.get(r'http://' + str(IP) + r'/Productlist/?access_token='+str(token)+r'&format=json')
-    print('  <<< PASS: completed testing with %s >>>' % test)
+    print('  <<< PASS: completed testing with %s >>> \n' % test)
 except Exception as e:
     print('  <<< FAIL: completed testing with %s >>> \n %s' % (test, e))
 
 try:
     test = 'Productlist with ID filter endpoint'
-    print('<<< Testing %s >>>' % test)
+    print(r'http://' + str(IP) + r'/Productlist/64681/?access_token='+str(token)+r'&format=json')
     authentication = requests.get(r'http://' + str(IP) + r'/Productlist/64681/?access_token='+str(token)+r'&format=json')
-    print('  <<< PASS: completed testing with %s >>>' % test)
+    print('  <<< PASS: completed testing with %s >>> \n' % test)
 except Exception as e:
     print('  <<< FAIL: completed testing with %s >>> \n %s' % (test, e))
