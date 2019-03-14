65a2dccca20817daaf00d1e38cc897e6f576b8e5
mike <michael.mountford@rkwltd.com>
2019-03-14 10:25:24 +0000

    linux friendly

---
+++ backEnd/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mike/python-environments/rkwTest/matt-mikes-practice/backEnd/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
