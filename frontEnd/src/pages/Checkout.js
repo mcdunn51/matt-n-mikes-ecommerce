c660b592dc46183c0c28b45a9e511044a3488f9d
Matt Dunning <Matt.dunning@SVG.local>
2019-03-26 12:58:12 +0000

    conditionally rendering on checkout page

--- frontEnd/src/pages/Checkout.js
+++ frontEnd/src/pages/Checkout.js
@@ -1,76 +1,96 @@
 import React, { Component } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 
 class Checkout extends Component {
-    componentDidMount() {
-        axios({
-            method: 'post',
-            url: 'http://100.1.253.16:8000/Address/?access_token=SDhm0d95wxYxnBzeFIEXL2Fbev14GW',
-            data: {
-                customerID: 1,
-                address1: "Sutton House",
-                address2: "Berry Hill Road",
-                town: "Staffordshire",
-                county: "Staffordshire",
-                postcode: "ST4 2NL",
-                phoneNumber: "01782 838822",
-                email: "rkw@rkwltd.com",
-                country: "UK",
-                city: 'stoke-on-trent'
-            }
-        })
-            .then(res => {
-                console.log(res.data);
-                // this.setState({ products: res.data });
-            })
-    }
+
+    // componentDidMount() {
+        // check if the person is logged in
+    //     if 
+
+    //     axios({
+    //         method: 'post',
+    //         url: 'http://100.1.253.16:8000/Address/?access_token=SDhm0d95wxYxnBzeFIEXL2Fbev14GW',
+    //         data: {
+    //             customerID: 1,
+    //             address1: "Sutton House",
+    //             address2: "Berry Hill Road",
+    //             town: "Staffordshire",
+    //             county: "Staffordshire",
+    //             postcode: "ST4 2NL",
+    //             phoneNumber: "01782 838822",
+    //             email: "rkw@rkwltd.com",
+    //             country: "UK",
+    //             city: 'stoke-on-trent'
+    //         }
+    //     })
+    //         .then(res => {
+    //             console.log(res.data);
+    //             // this.setState({ products: res.data });
+    //         })
+    // }
+
     render() {
+        const { isLoggedIn } = this.props
+        // upto here
+        console.log(isLoggedIn)
         return (
             <Container>
                 <h2>Checkout</h2>
-                <Row className="mt-5">
-                    <Col md={6} id="checkoutSummary">
-                        <Row>
-                            <Col md={2}>
-                                picture
-                            </Col>
-                            <Col md={4}>
-                                description
-                            </Col>
-                            <Col md={3}>
-                                Qty
-                            </Col>
-                            <Col md={3}>
-                                price
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={2}>
-                                picture
-                            </Col>
-                            <Col md={4}>
-                                description
-                            </Col>
-                            <Col md={3}>
-                                Qty
-                            </Col>
-                            <Col md={3}>
-                                price
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col md={6} id="addressCol">
-
-                    </Col>
-                </Row>
-            </Container>
+                 {isLoggedIn ? (
+                      <Row className="mt-5">
+                      <Col md={6} id="checkoutSummary">
+                          <Row>
+                              <Col md={2}>
+                                  picture
+                              </Col>
+                              <Col md={4}>
+                                  description
+                              </Col>
+                              <Col md={3}>
+                                  Qty
+                              </Col>
+                              <Col md={3}>
+                                  price
+                              </Col>
+                          </Row>
+                          <Row>
+                              <Col md={2}>
+                                  picture
+                              </Col>
+                              <Col md={4}>
+                                  description
+                              </Col>
+                              <Col md={3}>
+                                  Qty
+                              </Col>
+                              <Col md={3}>
+                                  price
+                              </Col>
+                          </Row>
+                      </Col>
+                      <Col md={6} id="addressCol">
+  
+                      </Col>
+                  </Row>      
+                    ) : (
+                        <h3>You need to be logged in to view the checkout page</h3>
+                    ) }
+                 </Container>       
+               
         )
     }
 
 }
 
+function mapStateToProps(state) {
+    return {
+        isLoggedIn: state.accessToken
+    }
+}
 
 
-export default Checkout;
+
+export default connect(mapStateToProps)(Checkout);
